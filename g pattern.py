0ac94ec48c4e36d0657da81b73c5fefa092ecee4
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-12 21:08:49 +0530

    G pattern
      ***  
     *   * 
    *      
    *   ***
    *     *
    *    * 
      ***

---
+++ g pattern.py
@@ -0,0 +1,7 @@
+for i in range(7):
+    for j in range(7):
+        if (i==0 and (j==2 or j==3 or j==4)) or (i==1 and(j==1 or j==5)) or (i==2 and j==0) or (i==3 and (j==0 or j==4 or j==5 or j==6)) or (i==4 and (j==6 or j==0)) or (i==5 and (j==0 or j==5)) or (i==6 and (j==2 or j==3 or j==4)):
+            print("*",end='')
+        else:
+            print(end=' ')
+    print()
