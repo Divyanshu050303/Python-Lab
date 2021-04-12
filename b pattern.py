88936b4288467ee93e2e6f51145168dd36b50f96
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-12 16:20:35 +0530

    b pattern
    ***** 
    *      *
    *      *
    ***** 
    *      *
    *      *
    *****

---
+++ b pattern.py
@@ -0,0 +1,7 @@
+for i in range(7):
+    for j in range(6):
+        if (i==0 and j<5) or (i==1 and (j==0 or j==5) ) or (i==2 and (j==0 or j==5)) or ( i==3 and j<5) or ( i==4 and (j==0 or j==5)) or ( i==5 and (j==0 or j==5)) or (i==6 and j<5):
+            print("*", end='')
+        else:
+            print(' ', end='')
+    print()
