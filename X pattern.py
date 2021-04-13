061f610d437500f103f6cd736edb8c21473636f9
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-13 15:54:07 +0530

    X pattern
    *       * 
    *       * 
      *   *   
        *     
      *   *   
    *       * 
    *       *

---
+++ X pattern.py
@@ -0,0 +1,7 @@
+for i in range(7):
+    for j in range(5):
+        if (j==0 and (i==0 or i==1 or i==5 or i==6)) or (j==4 and (i==0 or i==1 or i==5 or i==6)) or (i==2 and (j==1 or j==3)) or (i==4 and (j==1 or j==3)) or (i==3 and j==2):
+            print("*", end=' ')
+        else:
+            print(end='  ')
+    print()
