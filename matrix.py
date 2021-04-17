4c85ac5be8e261d9de6bd362a04804f018c7c9cc
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-17 20:50:58 +0530

    matrix entered by the user

---
+++ matrix.py
@@ -0,0 +1,17 @@
+r=int(input("Enter the number of row"))
+m=[]
+n=[]
+p=[]
+for i in range(r):
+    a=int(input("Enter the next element"))
+    m.append(a)
+for i in range(r):
+    a=int(input("Enter the next element"))
+    n.append(a)
+for i in range(r):
+    a=int(input("Enter the next element"))
+    p.append(a)
+array_input = [m, n, p]
+print(array_input[0])
+print(array_input[1])
+print(array_input[2])
