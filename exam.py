b7fea6351a8d4408623da5e924186313692c1b54
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-17 10:11:25 +0530

    you appear in exam or not

---
+++ exam.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the class held"))
+m=int(input("Enter the class attended by you "))
+v=(m/n)*100
+if v>=75:
+    print("You will appear in the exam")
+else:
+    print("You will not appear in the exam")
