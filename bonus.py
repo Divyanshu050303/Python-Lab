2a21cf90ef198dd23b63a165848c90b60e0cf534
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-17 10:08:31 +0530

    bonus in the company

---
+++ bonus.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the salary"))
+m=int(input("Enter the working year with the company"))
+if m>=5:
+    i=(n*5)/100
+    print("Your bouns is", i)
+else:
+    print("You work less than 5 year") 
