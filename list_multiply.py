4a434127c738916b3c2deb59978e147f463d7af5
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-04-18 20:56:02 +0530

    Whole list multiply

---
+++ list_multiply.py
@@ -0,0 +1,8 @@
+from functools import reduce
+
+n=[]
+num=int(input("Enter the length of list"))
+for i in range(num):
+    n.append(int(input("Enter the  next number")))
+su=reduce(lambda a, b: a*b, n)
+print(su)
