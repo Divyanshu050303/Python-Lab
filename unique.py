b121e951da252d73f859e8715517683cccecd19f
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-05-07 11:13:21 +0530

    like a set

--- unique.py
+++ unique.py
@@ -1,7 +1,9 @@
-n=int(input('Enter the number'))
-for i in range(n+1):
-    for j in range(1, i):
-        print(j, end='')
-    for j in range(i, 0, -1):
-        print(j, end='')
-    print()
+def lst(n):
+    s=[]
+    for i in n:
+        if i not in s:
+            s.append(i)
+    return s
+
+
+print(lst([1, 2, 3, 3, 3, 3, 4, 5]))
