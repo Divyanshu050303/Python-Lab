159896c365f149d4ff1c34774e3bc26a03da19d5
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-05-08 10:39:26 +0530

    width of the string

---
+++ width of simple text.py
@@ -0,0 +1,8 @@
+import textwrap
+n='''Python is a widely used high-level, general-purpose, interpreted,
+  dynamic programming language. Its design philosophy emphasizes
+  code readability, and its syntax allows programmers to express
+  concepts in fewer lines of code than possible in languages such
+  as C++ or Java.
+'''
+print(textwrap.fill(n, width=40))
