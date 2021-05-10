7569950a2377446021f3acc2f826d09252bc9657
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-05-10 09:05:42 +0530

    access first three number in the array

---
+++ first three number inthe array.py
@@ -0,0 +1,8 @@
+from array import *
+array_num=array('i', [1, 2, 4, 6, 7])
+for i in array_num:
+    print(i)
+print("Access first three item individually ")
+print("The first element in the array", array_num[0])
+print("The second element in the array", array_num[1])
+print("The third element in the array", array_num[2])
