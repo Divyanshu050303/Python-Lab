cead674485108f4a7c5cd3512de151bc47649c9b
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-05-14 11:11:49 +0530

    generate caption

---
+++ generate capture.py
@@ -0,0 +1,10 @@
+import random
+
+num=random.randint(0, 9)
+b=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z']
+a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z']
+c=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+al=random.choice(a)
+ac=random.choice(b)
+ad=random.choice(c)
+print(num, al, ac, ad, sep="")
