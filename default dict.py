3b3a9de7487dc138179ab838ac20dc95e97eee8f
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-05-25 07:15:22 +0530

    default dictionary  hacker rank solution

---
+++ default dict.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+n, m = map(int,input().split())
+
+a = defaultdict(list)
+for i in range(1, n + 1):
+    a[input()].append(i)
+
+for i in range(1, m + 1):
+    key = input()
+    if len(a[key]) > 0:
+        print(" ".join(str(c) for c in a[key]))
+    else:
+        print(-1)
