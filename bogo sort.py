2811db8b64ba78e62a3e69e4df8828306c0c0905
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-07-24 20:29:27 +0530

    bogo sort

---
+++ bogo sort.py
@@ -0,0 +1,23 @@
+import random
+def bogosort(a):
+    n=len(a)
+    while (is_sorted(a)==False):
+        shuffle(a)
+def is_sorted(a):
+    n=len(a)
+    for i in range(0, n-1):
+        if a[i]>a[i+1]:
+            return  False
+    return True
+def shuffle(a):
+    n=len(a)
+    for i in range(0, n):
+        r=random.randint(0, n-1)
+        a[i], a[r]=a[r], a[i]
+
+
+a=[3, 2, 4, 1, 0, 5]
+bogosort(a)
+print("Sorted array is :")
+for i in a:
+    print(i, end=' ')
