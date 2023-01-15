b781e4e4de0c21cfc6d8215ce8a41ffef5f01743
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-01-15 23:30:59 +0530

    recursive function

---
+++ recursive.py
@@ -0,0 +1,15 @@
+def recursive_fibonacci(n):
+  if n <= 1:
+      return n
+  else:
+      return(recursive_fibonacci(n-1) + recursive_fibonacci(n-2))
+ 
+n_terms = 10
+ 
+# check if the number of terms is valid
+if n_terms <= 0:
+  print("Invalid input ! Please input a positive value")
+else:
+  print("Fibonacci series:")
+for i in range(n_terms):
+    print(recursive_fibonacci(i))
