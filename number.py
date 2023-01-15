80ebe01ac3bd2313fe69577f417c4e6df5c646ba
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-01-15 22:26:54 +0530

    number

---
+++ number.py
@@ -0,0 +1,55 @@
+#  Number with Boolean Operator
+
+#  And Operator
+#  1.  if both the value is equal only then it gives us true
+#  2. if both the value is not equal ir gives us false
+
+#  both value is not equal
+
+if 3 == 5 and 5 == 3:
+    print(True)
+else:
+    print(False)
+
+if 5 == 3 and 3 == 5:
+    print(True)
+else:
+    print(False)
+
+if 5 == 15 and 45 == 13:
+    print(True)
+else:
+    print(False)
+
+#  both the value is equal
+if 5 == 5 and 3 == 3:
+    print(True)
+else:
+    print(False)
+
+#  Or Operator
+#  1. if both the value is equal then it gives us true
+#  2. if both the value is not equal only then  it gives us false
+
+if 3 == 5 or 3 == 3:
+    print(True)
+else:
+    print(False)
+
+if 5 == 5 or 3 == 5:
+    print(True)
+else:
+    print(False)
+
+if 5 == 5 or 3 == 3:
+    print(True)
+else:
+    print(False)
+
+
+#  only if both the value is  not equal
+if 5 == 15 or 45 == 13:
+    print(True)
+else:
+    print(False)
+
